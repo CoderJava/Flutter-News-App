97d5c56b4203d107cc03172ee196a1ed9fda57ae
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2020-04-12 21:21:15 +0700

    Create unit test failure_test.dart

---
+++ lib/core/error/exception.dart
@@ -0,0 +1,3 @@
+class CacheException implements Exception {}
+
+class ConnectionException implements Exception {}
---
+++ lib/core/network/network_info.dart
@@ -0,0 +1,14 @@
+import 'package:data_connection_checker/data_connection_checker.dart';
+
+abstract class NetworkInfo {
+  Future<bool> get isConnected;
+}
+
+class NetworkInfoImpl implements NetworkInfo {
+  final DataConnectionChecker dataConnectionChecker;
+
+  NetworkInfoImpl(this.dataConnectionChecker);
+
+  @override
+  Future<bool> get isConnected => dataConnectionChecker.hasConnection;
+}
