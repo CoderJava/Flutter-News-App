385be6ba85af0f94a5fba0611254eeabbb0b3fc6
CoderJava <kolonel.yudisetiawan@gmail.com>
2021-11-21 10:50:48 +0700

    Create use_case.dart
    This file is used as abstract class of all use case.

---
+++ lib/core/use_case/use_case.dart
@@ -0,0 +1,12 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_news_app/core/error/failure.dart';
+
+abstract class UseCase<Type, Params> {
+  Future<Either<Failure, Type>> call(Params params);
+}
+
+class NoParams extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
