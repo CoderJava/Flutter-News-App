be8e2b0d5e75e85280b3915605f1ac3cc713ad0b
CoderJava <kolonel.yudisetiawan@gmail.com>
2021-11-21 10:45:53 +0700

    Create network_info.dart file
    This file is used to check internet connection.

---
+++ lib/core/network/network_info.dart
@@ -0,0 +1,17 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+abstract class NetworkInfo {
+  Future<bool> get isConnected;
+}
+
+class NetworkInfoImpl implements NetworkInfo {
+  final Connectivity connectivity;
+
+  NetworkInfoImpl(this.connectivity);
+
+  @override
+  Future<bool> get isConnected async {
+    final connectivityResult = await connectivity.checkConnectivity();
+    return connectivityResult != ConnectivityResult.none;
+  }
+}
