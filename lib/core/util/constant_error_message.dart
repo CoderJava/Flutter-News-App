1f774f0bcdb48bfbf5e8367c72f7849b4e90f9e6
CoderJava <kolonel.yudisetiawan@gmail.com>
2021-11-21 10:53:43 +0700

    Create constant_error_message.dart file
    This file is used for template error message for case no internet connection

---
+++ lib/core/util/constant_error_message.dart
@@ -0,0 +1,6 @@
+class ConstantErrorMessage {
+  final connectionError = 'connectionError';
+  final connectionRefused = "'statusCode' was called on null";
+  final connectionTimedOut = 'Connection timed out';
+  final failureUnknown = 'Unknown';
+}
