0a088e7400c375fae9b7b6d35c8dc1f2af6afc07
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-06-13 08:29:55 +0700

    Set variable _instance to late and rename variable baseUrl to baseUrlNewsEndpoint

--- lib/config/flavor_config.dart
+++ lib/config/flavor_config.dart
@@ -6,9 +6,9 @@ enum Flavor {
 }
 
 class FlavorValues {
-  final String baseUrl;
+  final String baseUrlNewsEndpoint;
 
-  FlavorValues({required this.baseUrl});
+  FlavorValues({required this.baseUrlNewsEndpoint});
 }
 
 class FlavorConfig {
@@ -18,19 +18,19 @@ class FlavorConfig {
   final Color colorPrimaryDark;
   final Color colorPrimaryLight;
   final Color colorAccent;
-  final FlavorValues? values;
+  final FlavorValues values;
 
-  static FlavorConfig? _instance;
+  static late FlavorConfig _instance;
 
   factory FlavorConfig({
-    @required Flavor? flavor,
+    required Flavor? flavor,
     Color colorPrimary = Colors.blue,
     Color colorPrimaryDark = Colors.blue,
     Color colorPrimaryLight = Colors.blue,
     Color colorAccent = Colors.blueAccent,
-    @required FlavorValues? values,
+    required FlavorValues values,
   }) {
-    _instance ??= FlavorConfig._internal(
+    _instance = FlavorConfig._internal(
       flavor,
       enumName(flavor.toString()),
       colorPrimary,
@@ -39,7 +39,7 @@ class FlavorConfig {
       colorAccent,
       values,
     );
-    return _instance!;
+    return _instance;
   }
 
   FlavorConfig._internal(
@@ -52,14 +52,14 @@ class FlavorConfig {
     this.values,
   );
 
-  static FlavorConfig? get instance => _instance;
+  static FlavorConfig get instance => _instance;
 
   static String enumName(String enumToString) {
     final paths = enumToString.split('.');
     return paths[paths.length - 1];
   }
 
-  static bool isProduction() => _instance?.flavor == Flavor.production;
+  static bool isProduction() => _instance.flavor == Flavor.production;
 
-  static bool isDevelopment() => _instance?.flavor == Flavor.development;
+  static bool isDevelopment() => _instance.flavor == Flavor.development;
 }
