be817fbd08ec83c1363865ab3984968c47334886
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-06-13 08:31:21 +0700

    Add Dio and Helper to mock_helper.dart

--- test/helper/mock_helper.dart
+++ test/helper/mock_helper.dart
@@ -1,7 +1,11 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_news_app/core/util/helper.dart';
 import 'package:mockito/annotations.dart';
 
 @GenerateMocks([
   Connectivity,
+  Dio,
+  Helper,
 ])
 void main() {}
