3fff877287b46f31754d82205f08ba138132101d
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-06-15 08:19:02 +0700

    Add NewsRemoteDataSource and NetworkInfo to mock_helper.dart

--- test/helper/mock_helper.dart
+++ test/helper/mock_helper.dart
@@ -1,11 +1,15 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter_news_app/core/network/network_info.dart';
 import 'package:flutter_news_app/core/util/helper.dart';
+import 'package:flutter_news_app/feature/data/datasource/news/news_remote_data_source.dart';
 import 'package:mockito/annotations.dart';
 
 @GenerateMocks([
   Connectivity,
   Dio,
   Helper,
+  NewsRemoteDataSource,
+  NetworkInfo,
 ])
 void main() {}
