b53bcdd6f55cf80ba385b34dab90e52e5a7a274f
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-06-26 21:12:15 +0700

    Rename function generateRoute to be onGenerateRoute

--- lib/core/route/app_route.dart
+++ lib/core/route/app_route.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppRoute {
-  static Route<dynamic> generateRoute(RouteSettings settings) {
+  static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     Map<String, dynamic>? arguments;
     if (settings.arguments is Map<String, dynamic>?) {
       arguments = settings.arguments as Map<String, dynamic>?;
