32cb54e817381ccb7f7c70b0bc13cb9027c6ab1c
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-06-26 21:14:33 +0700

    Create splash_page.dart

---
+++ lib/feature/presentation/page/splash/splash_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_news_app/core/util/constant_value.dart';
+import 'package:flutter_news_app/core/util/images.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SplashPage extends StatelessWidget {
+  SplashPage({Key? key}) : super(key: key);
+
+  final constantColor = ConstantColor();
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaQueryData = MediaQuery.of(context);
+    final widthScreen = mediaQueryData.size.width;
+    return Scaffold(
+      backgroundColor: constantColor.primaryColor500,
+      body: Center(
+        child: SvgPicture.asset(
+          AssetSvg.logoFlutterNews,
+          width: widthScreen / 2,
+        ),
+      ),
+    );
+  }
+}
