414b0b0591877d2737352e1c35587f6ad63117c8
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-07-09 19:46:12 +0700

    Update assets svg

--- lib/core/util/images.dart
+++ lib/core/util/images.dart
@@ -1,4 +1,5 @@
 class AssetSvg {
   static const _iconPath = 'assets/svg';
-  static const logoFlutterNews = '$_iconPath/logo_flutter_news.svg';
+  static const logoFlutterNewsRounded = '$_iconPath/logo_flutter_news_rounded.svg';
+  static const logoFlutterNewsRectangle = '$_iconPath/logo_flutter_news_rectangle.svg';
 }
