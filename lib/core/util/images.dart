9122f24d6363c7aa3b0a7724a53de00024e20882
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-07-10 20:10:09 +0700

    Update images.dart
    Register newspaper image.

--- lib/core/util/images.dart
+++ lib/core/util/images.dart
@@ -1,5 +1,10 @@
-class AssetSvg {
+class AssetsSvg {
   static const _iconPath = 'assets/svg';
   static const logoFlutterNewsRounded = '$_iconPath/logo_flutter_news_rounded.svg';
   static const logoFlutterNewsRectangle = '$_iconPath/logo_flutter_news_rectangle.svg';
+}
+
+class AssetsImage {
+  static const _iconPath = 'assets/image';
+  static const newspaper = '$_iconPath/image_news_paper.jpeg';
 }
